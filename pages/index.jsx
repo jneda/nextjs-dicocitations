a8db5395d3bc1abf769073682c59bebd9f712432
jdane <julien.dane@gmail.com>
2023-02-01 13:11:34 +0100

    feat: add random quote in homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,20 @@
-import Link from "next/link";
-import Layout from "../components/layout";
-import SearchForm from "../components/SearchForm/SearchForm";
-import Author from "../models/author";
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import Layout from '../components/layout'
+import SearchForm from '../components/SearchForm/SearchForm'
+import Author from '../models/author'
+import Quote from '../models/quote'
 
-export default function HomePage({ authors }) {
+import styles from "./index.module.css";
+
+export default function HomePage({ authors, quote }) {
+
   return (
     <Layout>
       <section>
         <article>
-          <h1>Citation du jour</h1>
+          <p className={styles["quote-text"]}>{quote.text}</p>
+          <p className={styles["quote-author"]}>{`${quote.author.firstName} ${quote.author.lastName}`}</p>
         </article>
       </section>
 
@@ -17,20 +23,35 @@ export default function HomePage({ authors }) {
       <nav>
         <ul>
           <li>
-            <Link href="/quote/new" role="button" className="outline">Ajouter une citation</Link>
+            <Link href="/quote/new" role="button" className="outline">
+              Ajouter une citation
+            </Link>
           </li>
         </ul>
       </nav>
     </Layout>
-  );
+  )
 }
 
 export async function getServerSideProps() {
   let authors = await Author.findAll({
-    order: [['lastName', 'ASC'], ['firstName', 'ASC']]
-  });
-  authors = authors.map(author => author.toJSON());
+    order: [
+      ['lastName', 'ASC'],
+      ['firstName', 'ASC'],
+    ],
+  })
+  authors = authors.map((author) => author.toJSON())
+
+  let quotes = await Quote.findAll({ include: Author });
+  quotes = quotes.map((quote) => quote.toJSON())
+  const quote = getRandomQuote(quotes);
+
   return {
-    props: { authors }
+    props: { authors, quote },
   }
+}
+
+function getRandomQuote(quotes) {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
 }
